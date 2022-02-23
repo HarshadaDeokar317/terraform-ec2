eb68f5a9440f0f94c89d8e01f97a3f0165f616a5
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2022-02-23 12:00:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
-  region = var.aws_region
+  region = "eu-east-1"
+ # region = var.aws_region
 #   access_key = var.AWS_ACCESS_KEY
 # secret_key = var.AWS_SECRET_KEY
 }
