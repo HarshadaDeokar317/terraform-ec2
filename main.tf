b21bca923c04297ca9d283ea14ec8670ff0948c5
Harshada Deokar <46487473+HarshadaDeokar317@users.noreply.github.com>
2022-02-23 11:57:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-provider "aws" {
-  region = var.aws_region
-  access_key = var.AWS_ACCESS_KEY
-secret_key = var.AWS_SECRET_KEY
-}
+# provider "aws" {
+#   region = var.aws_region
+#   access_key = var.AWS_ACCESS_KEY
+# secret_key = var.AWS_SECRET_KEY
+# }
 
 #Create security group with firewall rules
 resource "aws_security_group" "my_security_group" {
